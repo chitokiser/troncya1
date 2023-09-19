9204197042f015f716b9c016941db4b31db13a2a
cya <daguri75@gmail.com>
2023-09-19 23:06:47 +0900

    2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
-import * as TronWeb from '/node_modules/tronweb/dist/TronWeb.node.js';
+const TronWeb = require('tronweb')
 const tronWeb = new TronWeb({
     fullHost: 'https://api.trongrid.io',
     headers: { "TRON-PRO-API-KEY": '0de2a20b-8ac7-4092-9ff4-75d9f9227056' },
     privateKey: 'c4cc526ccd122b6f044f82fa87f1349b3380e80b04535ea17fcfde97a4e1eb2f'
 })
+
 
 const contractAbi = {
   cyadex: [
